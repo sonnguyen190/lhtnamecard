62831c8051395d727d06a4509313a81b6ae3e854
sonnguyen190 <sonkute190@gmail.com>
2021-07-05 22:29:51 +0700

    điền nội dung commit vào đây

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className='fss'>   <h1> My Profile</h1>
+   <img className="image" src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t1.15752-9/210858460_1142921372867005_2600015718065390802_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=ae9488&_nc_ohc=Jtq4IE2WDR4AX-5pmUj&_nc_ht=scontent.fpnh22-1.fna&oh=b0dffb0faa51a5817f57381b1ab59150&oe=60E802B3" alt="" />
+   <h2>Lưu Hoài Thương</h2>
+   <h4>Đang Ép Ây</h4>
+   <img className='gif' src="https://img2.thuthuatphanmem.vn/uploads/2018/12/31/hinh-anh-dong-ngo-nghinh-de-thuong_112054335.gif" alt="" />
+
+   <div> <h4>Ai Đang Cô Đơn Tui Hốt Luôn Nà !! <br/>
+   </h4>
+    
+   </div>
+</div>
+
+   <div>
+    <ul className="ulli"> 
+
+      <li>
+     
+      <a className='linkfb' href="https://www.facebook.com/thuong.luu.75054689/"> <i class="bi bi-facebook"></i>  Facebook</a>
+      </li>
+
+      <li>
+      
+      <a className='linkfb' href="tel:0964427005"><i class="bi bi-telephone-fill"></i>  NumberPhone</a>
+      </li>
+
+      <li>
+      
+      <a className='linkfb' href="mailto:luuhoaithuongst@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a>
+      </li>
+
+    </ul>
+   </div>
+   © 2021 Namecard | Created by <a className='linkfb1' href="https://www.facebook.com/sonvjpro.nguyen">Sơn Nguyễn</a> 
     </div>
+    
   );
 }
 
