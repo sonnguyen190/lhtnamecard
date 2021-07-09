8697023a8011b39b05f0e7482e3abfc56bd50e1d
sonnguyen190 <sonkute190@gmail.com>
2021-07-09 20:08:51 +0700

    điền nội dung commit vào đây

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import anhthuong from './image/anhthuong.jpg';
 function App() {
   return (
     <div className="App" >
       <div className='fss'>   <h1> My Profile</h1>
-   <img className="image" src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t1.15752-9/210858460_1142921372867005_2600015718065390802_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=ae9488&_nc_ohc=Jtq4IE2WDR4AX-5pmUj&_nc_ht=scontent.fpnh22-1.fna&oh=b0dffb0faa51a5817f57381b1ab59150&oe=60E802B3" alt="" />
+   <img className="image" src={anhthuong} alt="" />
    <h2>Lưu Hoài Thương</h2>
    <h4>Đang Ép Ây</h4>
    <img className='gif' src="https://img2.thuthuatphanmem.vn/uploads/2018/12/31/hinh-anh-dong-ngo-nghinh-de-thuong_112054335.gif" alt="" />
@@ -18,18 +19,18 @@ function App() {
     <ul className="ulli"> 
 
       <li>
-     
-      <a className='linkfb' href="https://www.facebook.com/thuong.luu.75054689/"> <i class="bi bi-facebook"></i>  Facebook</a>
+     <button className='button'><a className='linkfb' href="https://www.facebook.com/thuong.luu.75054689/"> <i class="bi bi-facebook"></i>  Facebook</a></button>
+      
       </li>
 
       <li>
+      <button className='button'><a className='linkfb' href="tel:0964427005"><i class="bi bi-telephone-fill"></i>  Phone</a></button>
       
-      <a className='linkfb' href="tel:0964427005"><i class="bi bi-telephone-fill"></i>  NumberPhone</a>
       </li>
 
       <li>
+      <button className='button'><a className='linkfb' href="mailto:luuhoaithuongst@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a></button>
       
-      <a className='linkfb' href="mailto:luuhoaithuongst@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a>
       </li>
 
     </ul>
